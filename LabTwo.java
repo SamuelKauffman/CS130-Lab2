6ed3e1a8ca52f818e12e04a154daf5ec924f898e
SamuelKauffman <112411760+SamuelKauffman@users.noreply.github.com>
2022-09-08 09:39:57 -0400

    Add files via upload

---
+++ LabTwo.java
@@ -0,0 +1,21 @@
+public class LabTwo
+{
+	public static void main(String[] args)
+	{
+	
+		System.out.println("On Monday I wake up at" + " 8" + "am.\n"
+		 + "I take a shower.\n"
+		 + "I then brush my teeth.\n"
+		 + "I head to my" + " 9" + "am class.\n"
+		 + "I learn about the Economy.\n"
+		 + "Then I get breakfast.\n"
+		 + "I normally get a coffee and a bagel.\n"
+		 + "I head to my cs class.\n"
+		 + "I learn Java.\n"
+		 + "Then I go to my English class.\n"
+		 + "I am finally done with my classes and get to relax\n");
+		System.out.println("*\n" + "**\n" + "***\n" + "****\n" + "*****\n");
+		System.out.println("*********\n" + " ******* \n" + "  *****  \n" + "   ***   \n" + "    *    \n");
+		System.out.println("Bellarmine University\n" + "2001 Newburg Road\n" + "Louisville, Kentucky 40205\n");
+	}
+}
